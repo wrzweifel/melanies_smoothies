b616f9f6733fd5c7cec05927cbb541b8eab31d33
wrzweifel <wrzweifel@hotmail.com>
2025-01-07 10:37:04 -0600

    Update streamlit_app.py
    fixed session typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.write(
 )
 from snowflake.snowpark.functions import col
 
-cnx = st.connection("snowflake"
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
