18f504966ab5209c2203788ae6ca8ddddbb1db6c
wrzweifel <wrzweifel@hotmail.com>
2025-01-07 16:12:42 -0600

    Update streamlit_app.py
    moved to inblock

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ st.write(
     """
 )
 from snowflake.snowpark.functions import col
+import requests
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('name:', name_on_order)
@@ -24,6 +25,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -37,7 +41,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered ' + name_on_order + '!', icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+
