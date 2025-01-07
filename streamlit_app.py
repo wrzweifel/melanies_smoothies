e684b8a1dc66731b60009b590b94fd9764267788
wrzweifel <wrzweifel@hotmail.com>
2025-01-07 16:03:03 -0600

    Update streamlit_app.py
    updated with missing button

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,16 @@ st.write(
 )
 from snowflake.snowpark.functions import col
 
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('name:', name_on_order)
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
-ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe)
+ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe
+                                 , max_selections = 5)
 
 if ingredients_list:
     ingredients_string = ''
@@ -22,13 +26,14 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
     #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
 
     #st.write(my_insert_stmt)
+    
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
+        st.success('Your Smoothie is ordered ' + name_on_order + '!', icon="✅")
 
